1a4d17ab78518d5f45301de339ad3f5aac73b940
hozay22 <jbarajas040@gmail.com>
2023-07-27 13:42:59 -0700

    added function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var number = 1;
-var mouseClicked = function() {
+var addNumber = function() {
   console.log('mouseClick = ' + number);
   number++;
+}
+
+var name = "jose barajas";
+var myName = function() {
+  console.log('mouseClick = ' + name) 
 }
